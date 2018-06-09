443bc990b3153f58327c11fb8a8a896a61b9b05e
Ari Herman <ari.j.herman@gmail.com>
2018-06-09 10:58:53 -0700

    Puts graph functions in separate file plus other stuff

---
+++ GraphFun.py
@@ -0,0 +1,52 @@
+import numpy as np
+import random
+import networkx as nx
+import networkx.algorithms.approximation as nxaa
+import matplotlib.pyplot as plt
+import networkx.generators.directed
+import itertools as it
+
+dtype = "int32"
+
+def sigma(X,T):
+    return 1./(1+np.exp(-X/T))
+    
+def isIndependent(a,A):
+    return np.dot(a,np.dot(A,a.T))==0
+   
+def genJohnsonGraph(v,k,i):
+    vset = ''.join([chr(c) for c in range(v)]) # MUST BE IN ALPHABETICAL ORDER STARTING WITH A!!!
+    combos=list(it.combinations(vset, k))
+    edges=[]
+    combos=[''.join(t[0] for t in x) for x in combos]
+    for idx,x in enumerate(combos):
+        for y in combos[idx:]:
+            if len(set(x) & set(y))==i:
+                edges.append((x,y))
+                edges.append((y,x))
+    G = nx.empty_graph(0, create_using=nx.DiGraph())
+    G.add_edges_from(e for e in edges)
+    return G
+
+def getAdjArray(G):
+    return np.array(nx.to_numpy_matrix(G)).astype(dtype)
+
+def findIndSet(A,niters):
+    N = np.shape(A)[0]
+    a = np.zeros((N,),dtype=dtype) # Initial active nodes
+    z = np.zeros((N,),dtype=dtype) # This is kept equal to A*a 
+    for k in np.linspace(-2,2,niters): # Run network while annealing temperature
+        T = np.exp(-k)
+        idx = np.random.permutation(N)
+        no_change = True # Does a change at all in this iteration?  This is not currently being used.
+        for i in idx:
+            new = np.random.random() < sigma(-2*z[i]+1,T)
+            old = a[i]
+            if old != new:     
+                if new<old:
+                    z -= A[i]
+                else:
+                    z += A[i]
+                a[i] = new
+                no_change = False
+    return a
--- IndependentSetsFinder.py
+++ IndependentSetsFinder.py
@@ -15,59 +15,17 @@ import matplotlib.pyplot as plt
 import networkx.generators.directed
 import itertools as it
 import time as tm
+from GraphFun import *
 
 dtype = "int32"
 niters = 100 # Number of iterations
-ntrials = 10 # Number of times to repeat algorithm
-
-def sigma(X,T):
-    return 1./(1+np.exp(-X/T))
-    
-def isIndependent(a,A):
-    return np.dot(a,np.dot(A,a.T))==0
-   
-def genJohnsonGraph(v,k,i):
-    vset = ''.join([chr(c) for c in range(v)]) # MUST BE IN ALPHABETICAL ORDER STARTING WITH A!!!
-    combos=list(it.combinations(vset, k))
-    edges=[]
-    combos=[''.join(t[0] for t in x) for x in combos]
-    for idx,x in enumerate(combos):
-        for y in combos[idx:]:
-            if len(set(x) & set(y))==i:
-                edges.append((x,y))
-                edges.append((y,x))
-    G = nx.empty_graph(0, create_using=nx.DiGraph())
-    G.add_edges_from(e for e in edges)
-    return G
-
-def getAdjArray(G):
-    return np.array(nx.to_numpy_matrix(G)).astype(dtype)
-
-def findIndSet(A,niters):
-    N = np.shape(A)[0]
-    a = np.zeros((N,),dtype=dtype) # Initial active nodes
-    z = np.zeros((N,),dtype=dtype) 
-    for k in np.linspace(-2,2,niters): # Run network while annealing temperature
-        T = np.exp(-k)
-        idx = [j for j in range(N)]
-        random.shuffle(idx)
-        for i in idx:
-            new = np.random.random() < sigma(-2*z[i]+1,T)
-            old = a[i]
-            if old != new:     
-                if new<old:
-                    z -= A[i]
-                    a[i] = new
-                else:
-                    z += A[i]
-                    a[i] = new
-    return a
+ntrials = 100 # Number of times to repeat algorithm
 
 ##################
 # Adjacency matrix
 ##################
 # Generalize Johnson graph
-v,k,i = 15,7,0
+v,k,i = 11,5,0
 G = genJohnsonGraph(v,k,i)
 A = getAdjArray(G)
 
@@ -92,32 +50,27 @@ beta = 0
 betas = []
 for m in range(ntrials): # Do best of 50 attempts
     a = findIndSet(A,niters)
-    betas.append(np.sum(a))
-    if np.sum(a)>beta:
-        best_set = a
-        beta = np.sum(a)  
+    if isIndependent(a,A):
+        betas.append(np.sum(a))
+        if np.sum(a)>beta:
+            best_set = a
+            beta = np.sum(a)  
 toc = tm.time()
 
+# Results
 print("Run time: "+str(toc-tic)+"s")
-print("Independent set computed by stochastic algorithm")
-print(best_set)
-print("Is it actually independent?")
-if isIndependent(a,A):
-    print("YES")
-else:
-    print("NO")
 print("Independence number according to the stochastic algorithm: " + str(beta))
 
-
-#prints the independent set
 IS = np.array([[ord(c) for c in G.nodes()[i]] for i in np.where(best_set)[0]],dtype=dtype)
+print("Independent set computed by stochastic algorithm")
+print(IS,"\n") # Print indices
+
 '''
 There's got to be a better way to do this part
 '''
 IS_bin = np.zeros((beta,v),dtype=dtype)
 for k in range(beta):
     IS_bin[k][IS[k]]=1
-print(IS,"\n") # Print indices
 #print(IS_bin) # Print binary
 
 ## Histogram of independent set sizes (requires seaborn)

72159c994bb78a79cc0ba1ffc28e966be443283a
Ari Herman <ari.j.herman@gmail.com>
2018-06-22 18:55:34 -0700

    WIP

--- IndependentSetsFinder.py
+++ IndependentSetsFinder.py
@@ -25,7 +25,7 @@ ntrials = 30 # Number of times to repeat algorithm
 # Adjacency matrix
 ##################
 # Generalize Johnson graph
-v,k,i = 15,7,0
+v,k,i = 9,4,0
 G = genJohnsonGraph(v,k,i)
 A = getAdjArray(G)
 

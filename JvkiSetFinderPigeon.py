802a0d2fd87746803a758459abd6f2e8b058e80b
taiyoshoe <tsterada@gmail.com>
2018-07-11 14:54:01 -0700

    JvkSetFinderPigeon.py

--- JvkiSetFinderPigeon.py
+++ JvkiSetFinderPigeon.py
@@ -1,4 +1,10 @@
 # Make array of Jvki graph independence numbers
+# THis takes advantage of the fact that if  k>v//2+i//2+1 then
+# J(v,k,i) will have no indpendent sets, I think?
+#however it wont produce the actual indpendent sets, to save time,
+# it would just be all vertices in that case, so we could have it do that later
+
+
 # Can't take advantage of J(v,k,i)-J(v,v-k,v-2k+i) symmetry as easily, 
 # so I wonder if it is instead worth it to save a 3D-array with values, and
 #load and update that appropriately, and just make a function in Graph fun
@@ -11,6 +17,7 @@ import networkx.algorithms.approximation as nxaa
 import matplotlib.pyplot as plt
 import networkx.generators.directed
 import itertools as it
+import scipy.special as scis
 import time as tm
 #from GraphFun import *
 
@@ -28,24 +35,27 @@ ii=1#intersection number
 # Load arrays
 alphas = np.loadtxt("IndependentSets/Jvk"+str(ii)+"AlphasTentative.txt",dtype=dtype)
 #max_v = np.shape(alphas)[0]
-maxIndSets = np.load("IndependentSets/maxJvk"+str(ii)+"IndSetsTentative.npy")
+#maxIndSets = np.load("IndependentSets/maxJvk"+str(ii)+"IndSetsTentative.npy")
 
 # Compute independent sets
-for v in range(max_v):
+for v in range(1,max_v):
     print(v)
-    for k in range(1,v+1):
+    for k in range(1,v//2+i//2+2):
 
         # Get Johnson adjacency array
         V,B = genJohnsonAdjList(v,k,ii)
         
         # Find an independent set
         maxIndSetIndicator = fastFindIndSet(B,niters,ntrials,start=-2,stop=2)
-        maxIndSet = np.array([V[i] for i in np.where(maxIndSetIndicator)[0]],dtype=dtype)
+#        maxIndSet = np.array([V[i] for i in np.where(maxIndSetIndicator)[0]],dtype=dtype)
         alpha = np.sum(maxIndSetIndicator)
 
         # If best so far...
         if alpha>alphas[v,k]:
-            maxIndSets[v,k] = maxIndSet
+#            maxIndSets[v,k] = maxIndSet
             alphas[v,k] = alpha
-            np.savetxt("IndependentSets/Jvk"+str(ii)+"AlphasTentative.txt",alphas,fmt='%5d',newline='\r\n')
-            np.save("IndependentSets/maxJvk"+str(ii)+"IndSetsTentative.npy",maxIndSets)
+            np.savetxt("IndependentSets/Jvk"+str(ii)+"AlphasPigeon.txt",alphas,fmt='%5d',newline='\r\n')
+#            np.save("IndependentSets/maxJvk"+str(ii)+"IndSetsTentative.npy",maxIndSets)
+    for k in range(v//2+i//2+2,v+1):
+        alphas[v,k] = scis.binom(v, k)
+        np.savetxt("IndependentSets/Jvk"+str(ii)+"AlphasPigeon.txt",alphas,fmt='%5d',newline='\r\n')

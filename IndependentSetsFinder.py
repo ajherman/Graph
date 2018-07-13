9ea10b93c41983cde2936e648d2dd22fe1e9dc47
Ari Herman <ari.j.herman@gmail.com>
2018-07-13 16:03:22 -0700

    WIP

--- IndependentSetsFinder.py
+++ IndependentSetsFinder.py
@@ -9,10 +9,10 @@ Created on Mon Feb 26 15:14:19 2018
 
 import numpy as np
 import random
-import networkx as nx
-import networkx.algorithms.approximation as nxaa
-import matplotlib.pyplot as plt
-import networkx.generators.directed
+#import networkx as nx
+#import networkx.algorithms.approximation as nxaa
+#import matplotlib.pyplot as plt
+#import networkx.generators.directed
 import itertools as it
 import time as tm
 from GraphFun import *
@@ -25,8 +25,8 @@ ntrials = 100 # Number of times to repeat algorithm
 # Adjacency matrix
 ##################
 # Generalize Johnson graph
-v,k,i = 13,5,4 #17,8,0 #13,5,4 # 19,9,8
-#V,A = genJohnsonAdjList(v,k,i)
+v,k,i = 17,8,0 #17,8,0 #13,5,4 # 19,9,8
+V,A = genJohnsonAdjList(v,k,i)
 
 ## Random
 #N = 1000 # Number of vertices
@@ -44,7 +44,19 @@ v,k,i = 13,5,4 #17,8,0 #13,5,4 # 19,9,8
 ######################
 # Super fast version
 tic = tm.time()
-best_set = fastFindIndSet(A,niters,ntrials,-2,1.5)
+best_set = fastFindIndSetAlt(A,niters,ntrials,-2,2)
+beta = np.sum(best_set)
+toc = tm.time()
+print("Run time for fast version: "+str(toc-tic)+"s")
+print("Independence number according to the stochastic algorithm: " + str(beta))
+#print("Best set: ")
+#print(best_set)
+IS = np.array([np.where(V[i])[0] for i in np.where(best_set)[0]],dtype=dtype)
+print("Independent set computed by stochastic algorithm")
+print(IS,"\n") # Print indices
+
+tic = tm.time()
+best_set = fastFindIndSet(A,niters,ntrials,-2,2)
 beta = np.sum(best_set)
 toc = tm.time()
 print("Run time for fast version: "+str(toc-tic)+"s")

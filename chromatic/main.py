4cbec79fa81c665cc91f7610eeb432eb01e42648
Ari Herman <ajherman@login2.cluster>
2024-03-08 12:02:03 -0800

    wip

--- chromatic/main.py
+++ chromatic/main.py
@@ -16,36 +16,37 @@ matplotlib.rcParams['animation.convert_path'] = 'magick'
 parser = argparse.ArgumentParser()
 parser.add_argument("--n-steps", type=int, default=800, help="Number of steps")
 parser.add_argument("--c", type=int, default=9, help="Number of colors")
-parser.add_argument("--beta", type=float, default=0.02, help="Temperature")
 parser.add_argument("--T", type=float, default=0.02, help="Temperature")
 parser.add_argument("--lam", type=float, default=1.0, help="Regularization parameter")
 parser.add_argument("--representation", type=str, default="softmax", help="Representation")
 parser.add_argument("--lr", type=float, default=0.1, help="Learning rate")
 parser.add_argument("--batch-size", type=int, default=10, help="Batch size")
 parser.add_argument("--make-movie", action="store_true", help="Make movie")
+parser.add_argument("--vki", nargs='+', type=int, default=[5, 2, 0], help="v, k, i")
 args = parser.parse_args()
 
 
 
 n_steps = args.n_steps
 c = args.c
-# beta = args.beta
 T = args.T
 lam = args.lam
 representation = args.representation
 lr = args.lr
 B=args.batch_size
 make_movie = args.make_movie
+v,k,i = args.vki
+
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print("Device: ",device)
+print("Device: ",device,"\n")
 
-G = genJohnsonGraph(10,4,3)
+G = genJohnsonGraph(v,k,i)
 A = getAdjArray(G)
 A = torch.tensor(A,dtype=torch.float32, device=device)
 N = A.shape[0] # Number of vertices
 
-mask = torch.ones(N,N,device=device)
+mask = torch.zeros(N,N,device=device) # Initialize with zeros, no learning happens
 
 # Init x
 if representation == "softmax":
@@ -53,84 +54,67 @@ if representation == "softmax":
     x.data = 10*x.data
 elif representation == "quantum":
     x = torch.randn((B,N,c),requires_grad=True, device=device) # Logits
-    # x.data = 10*x.data
-    x.data = torch.nn.functional.normalize(x.data,dim=-1)
-
-elif representation == "experimental":
-    x = torch.randn((B,N,c),requires_grad=True, device=device) # Logits
-    x.data = 10*x.data
-    x.data = torch.nn.functional.normalize(x.data,dim=-1)
-
+    x.data = torch.nn.functional.normalize(x.data,dim=-1,p=2)
+elif representation == "literal":
+    x = torch.rand((B,N,c),requires_grad=True, device=device)
+    x.data = torch.nn.functional.normalize(x.data,dim=-1,p=1)
     
 # Optimizer
 optimizer = torch.optim.Adam([x], lr=lr)
 # optimizer = torch.optim.SGD([x], lr=lr)
-
 
 # Minimize loss
 vals = []
 points =[]
 losses = []
 
-## Stuff I'm working on...
-# # def edge_penalty(p):
-# #     return 0
-# def f(x):
-#     # return (2*torch.asin(x)/np.pi)**2
-#     return x**2
-#     # return 1-torch.sqrt(1-x**2)
-#     # return x**2/(1-x**2)
-
-# Ts = torch.linspace(T,0.02,n_steps)
-# Ts = torch.logspace(2,-1,n_steps)
-for step in range(n_steps):
-# while T>0.01:
+for step in range(n_steps+1):
     optimizer.zero_grad()
 
     # Get probability
     if representation == "softmax":
-        beta = 1/T
-        y = torch.exp(beta*x)
-
+        y = torch.exp(x/T)
     elif representation == "quantum":
-        beta = 1/T
         y = x**2
-    # elif representation == "absolute":
-    #     beta = 1/T
-    #     y = torch.abs(x)
+    elif representation == "literal":
+        y = torch.abs(x)
 
-    if representation == "experimental":
-        q = y**2
-        p = q/torch.sum(q,dim=-1,keepdim=True)
-    else:
+    if representation == "quantum" or "literal":
         p = y/torch.sum(y,dim=-1,keepdim=True)
-    # print(torch.min(y@y.transpose(-1,-2)))
-    # print(torch.max(y@y.transpose(-1,-2)))
+    else:
+        p = y
 
     if make_movie:
         points.append(p.detach()) # Collect points
 
-
-    # Calculate loss
+    # Calculate loss (without masking, this should be equal to the number of edge violations...)
     multi_loss = 0.5*torch.sum((mask*A)*(p@p.transpose(-1,-2)),dim=(-1,-2)) # Covariance matrix
     # multi_loss = torch.sum(A*((p@p.transpose(-1,-2)))**3,dim=(-1,-2)) # Covariance matrix
     # multi_loss = torch.sum(A*(torch.asin(p@p.transpose(-1,-2))),dim=(-1,-2)) # Covariance matrix # Unstable
 
-        # z = p #torch.abs(x)
-        # w = torch.abs(x)
-        # z = w/torch.sqrt(torch.sum(w**2,dim=-1,keepdim=True))
-        # multi_loss = torch.sum((A*(z@z.transpose(-1,-2)))**2,dim=(-1,-2)) # Covariance matrix
-        # multi_loss = torch.sum((A*(torch.asin(z@z.transpose(-1,-2))*2/np.pi)**2),dim=(-1,-2)) # Covariance matrix
-        # multi_loss = torch.sum((A*f(z@z.transpose(-1,-2))),dim=(-1,-2)) # Covariance matrix
+    # z = p #torch.abs(x)
+    # w = torch.abs(x)
+    # z = w/torch.sqrt(torch.sum(w**2,dim=-1,keepdim=True))
+    # multi_loss = torch.sum((A*(z@z.transpose(-1,-2)))**2,dim=(-1,-2)) # Covariance matrix
+    # multi_loss = torch.sum((A*(torch.asin(z@z.transpose(-1,-2))*2/np.pi)**2),dim=(-1,-2)) # Covariance matrix
+    # multi_loss = torch.sum((A*f(z@z.transpose(-1,-2))),dim=(-1,-2)) # Covariance matrix
 
-    if step%100==0:
+    # Masking
+    if step%500==0:
         best = torch.min(multi_loss)
-        print(f"Step {step}: Loss: {multi_loss.mean().item():.2f} Best: {best.item():.2f}")
-        if step<0.9*n_steps:
+        avg = torch.mean(multi_loss)
+        print(f"Step {step}: Average loss: {avg.item():.2f} Min loss: {best.item():.2f}\n")
+        if False: #step<0.9*n_steps:
             # mask = (torch.rand_like(A) < 0.9).float()
             # mask = 1-0.2*torch.rand_like(A)
             # mask = 1-0.4*torch.rand_like(A)
+
             mask = 1-(1-step/n_steps)*torch.rand_like(A)
+            # mask = (torch.rand_like(A)>(1-step/n_steps)*torch.rand_like(A)).float()
+            # mask = (torch.rand_like(A)<step/n_steps).float()
+
+
+            print("Still masking")
         else:
             mask = torch.ones_like(A)
         torch.cuda.empty_cache()
@@ -140,15 +124,12 @@ for step in range(n_steps):
         
     loss = torch.sum(multi_loss) 
     loss.backward()
-    # torch.nn.utils.clip_grad_norm_([x], max_norm=0.1,norm_type='inf')
-    optimizer.step()
-    if representation == "quantum" or "experimental":
-        x.data = torch.nn.functional.normalize(x.data,dim=-1)
-    
 
-# print("Loss: ",loss.item())
-# print("Independence: ",torch.sum(A*(p@p.t())).item())
-# print("")
+    optimizer.step()
+    if representation == "quantum":
+        x.data = torch.nn.functional.normalize(x.data,dim=-1,p=2)
+    elif representation == "literal":
+        x.data = torch.nn.functional.normalize(x.data,dim=-1,p=1)
     
 
 # Two versions
@@ -167,33 +148,35 @@ for step in range(n_steps):
 #     y = torch.exp(beta*x)
 #     coloring = y/torch.sum(y,dim=1,keepdim=True)
 
-coloring = torch.softmax(100*torch.abs(p),dim=-1)
 ##################################
+_, max_indices = torch.max(p, dim=-1)
+colorings = torch.nn.functional.one_hot(max_indices, num_classes=c).float()
+minmax=colorings.max(-1)[0].min()
+if minmax.item()<0.999:
+    print("minmax",minmax.item())
+    print("Warning: Not a valid coloring")
 
-minmax=coloring.max(1)[0].min(0)[0]
-
-print(minmax)
-# coloring = torch.softmax(beta*x,dim=1)
-# print(p)
-# print(p>0.5)
-val = torch.sum(A*(coloring@coloring.transpose(1,2)),dim=(-1,-2))
-best,idx=torch.min(val,0)
-# print("Final losses: ",val)
+edge_violations = 0.5*torch.sum(A*(colorings@colorings.transpose(1,2)),dim=(-1,-2))
+best,idx=torch.min(edge_violations,0)
 print("Best: ",best.item())
+
 if best<10.1:
-    np.save('best_'+str(int(best.item()))+'.npy',p>0.5)
+    np.save('best_'+str(int(best.item()))+'.npy',p[idx].cpu().numpy())
+
+# Get class sizes
+best_coloring = colorings[idx]
+class_sizes = torch.sum(best_coloring,dim=0)
+print("Class sizes: ",class_sizes)
+print("Class proportions: ",class_sizes/N)
 
 if make_movie:
     points = torch.stack([point[idx] for point in points])
 
     # Get 
-    coloring = torch.softmax(50*points,dim=-1)  # Get actual colorings
+    coloring = torch.softmax(100*points,dim=-1)  # Get actual colorings
     points = points.cpu().numpy() # Collect points from best trial
     losses = torch.round(0.5*torch.sum(A*(coloring@coloring.transpose(-1,-2)),dim=(-1,-2))).int()
     losses = losses.cpu().numpy()
-
-    # losses = torch.stack([loss[idx] for loss in losses]).cpu().numpy() # Collect losses from best trial
-
 
     # Create random matrix the projects to 2 dimensions from N dimensions
     if c>3:
--- chromatic/submit.sh
+++ chromatic/submit.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 #SBATCH --job-name my_job
-#SBATCH --ntasks 10
-#SBATCH --nodes 1
+#SBATCH --ntasks 3
+#SBATCH --nodes 3
 #SBATCH --partition gpu
-#SBATCH --output output/result.txt
-#SBATCH --error output/result.err
-#SBATCH --gres=gpu:1
-python3 -u main.py --T 4 --c 9 --representation quantum --batch-size 5000 --n-steps 1000 --make-movie
-# python3 -u main.py --T 4 --c 9 --representation softmax --batch-size 5000 --n-steps 1000
+#SBATCH --gres=gpu:3
+#conda activate pytorch
+
+# srun -o output/test7.out --ntasks=1 -N 1 python3 -u main.py --c 7 --representation quantum --batch-size 5000 --n-steps 5000 &
+
+# srun -o output/test8.out --ntasks=1 -N 1 python3 -u main.py --c 8 --representation quantum --batch-size 5000 --n-steps 5000 &
+
+# srun -o output/test9.out --ntasks=1 -N 1 python3 -u main.py --c 9 --representation quantum --batch-size 5000 --n-steps 5000 &
+
+srun -o output/quantum.out --ntasks=1 -N 1 python3 -u main.py --vki 10 4 3 --c 9 --representation quantum --batch-size 1000 --n-steps 5000 --make-movie &
+
+# srun -o output/literal.out --ntasks=1 -N 1 python3 -u main.py --vki 10 4 3 --c 9 --representation literal --lr 0.5 --batch-size 5000 --n-steps 5000 &
+
+wait
+
 
 
